1e652ea5117c8e7ce466be99987e3ff3b68a17f0
James Bowes <jbowes@repl.ca>
2019-05-17 12:48:26 -0300

    Add some more godoc

--- doc.go
+++ doc.go
@@ -1,5 +1,12 @@
 /*
 Package cling provides a clear and obvious error wrapping API for Go 2/1.13+
 error values.
+
+Errors created by this package support the Go 2 proposal for error values:
+	https://golang.org/design/29934-error-values
+Most functionality in this package is available through the golang.org/x/xerrors
+package, or in errors itself after Go 1.13. However, this package provides an
+API that may be more familiar and comfortable to users of other error packages,
+like pkg/errors.
 */
 package cling
